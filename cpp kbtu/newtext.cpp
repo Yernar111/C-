d668535cd9ffce2120dc13c61e59080b2fb11f97
Yernar111 <ernarsuinullaev1@gmail.com>
2025-05-09 15:34:18 +0500

    new1

--- cpp kbtu/newtext.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-/* Кодировка файла abcde */
-int main(){
-    int n;
-    cin>>n;
-    cout<<n+1;
-    return 0;
-}
--- rec_является_ли_число_степенью_двойки.cpp
+++ rec_является_ли_число_степенью_двойки.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 string f(int n){
-    if (n==2){
+    if (n==1){
         return "YES";
     }
-    if (n==5 || n==3){
+    if (n%2==1){
         return "NO";
     }
     return f(n/2);
